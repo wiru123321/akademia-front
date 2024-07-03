35c471dc0e6f9aaf146d175241777af2c9359147
wiru123321 <wojtekmaj2@o2.pl>
2024-07-03 12:55:22 +0200

    add lang

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -6,11 +6,12 @@ import { FALLBACK_SEO } from "app/utils/constants";
 type Props = {
     params: {
         slug: string;
+        lang: string;
     };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const page = await getPageBySlug(params.slug);
+    const page = await getPageBySlug(params.slug, params.lang);
 
     if (!page.data[0]?.attributes?.seo) return FALLBACK_SEO;
     const metadata = page.data[0].attributes.seo;
@@ -22,7 +23,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export default async function PageRoute({ params }: Props) {
-    const page = await getPageBySlug(params.slug);
+    const page = await getPageBySlug(params.slug, params.lang);
     if (page.data.length === 0) return null;
     const contentSections = page.data[0].attributes.contentSections;
     return contentSections.map((section: any, index: number) =>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { sectionRenderer } from "./utils/section-renderer";
 import { getPageBySlug } from "app/utils/get-page-by-slug";
 
-export default async function RootRoute() {
+export default async function RootRoute({
+    params,
+}: {
+    params: { lang: string };
+}) {
     try {
-        const page = await getPageBySlug("home");
+        const page = await getPageBySlug("home", params.lang);
         if (page.error && page.error.status == 401)
             throw new Error(
                 "Missing or invalid credentials. Have you created an access token using the Strapi admin panel? http://localhost:1337/admin/"
